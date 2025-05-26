e3b43284d11cfc4a7d0c380743f85c01410acc0b
enjore1201 <shawn630730@gmail.com>
2025-05-26 13:40:54 +0700

    Update settings.py
    시크릿 키 수정

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ry=9!e3f@azkalp1to$4me=&m4wv^j%i$0!qvb=#&ul5sma@*7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
